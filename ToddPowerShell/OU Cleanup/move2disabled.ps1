4ead9ac61052ee0a74e50fc25c4fa6af4784888e
Todd Hammer <mihammer@microsoft.com>
2018-09-24 09:24:42 -0400

    commit

--- ToddPowerShell/OU Cleanup/move2disabled.ps1
+++
@@ -1,8 +0,0 @@
-#Move computers to the Disabled Computers OU
-
-$Disabled = GC "disabled.txt"
-ForEach($computer in $Disabled) 
-{
-# echo $computer    
-get-adcomputer $computer | move-adobject -targetpath "OU=Disabled Computers,DC=nasa,DC=group,DC=atlascopco,DC=com"
-}
